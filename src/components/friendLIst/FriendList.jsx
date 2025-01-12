41b488401d66e40f0e9506af87487c0638b13112
Yuliia Shvets <y.shvets85@gmail.com>
2025-01-12 13:18:56 +0100

    add .

--- src/components/friendLIst/FriendList.jsx
+++ src/components/friendLIst/FriendList.jsx
@@ -5,20 +5,16 @@ const FriendList = ({friends}) => {
   return (
       <div className={styles.friendSection}>
       <ul className={styles.friendList}>
-      {friends.map(({ avatar, name, isOnline }) => (
-         <> 
-<li className={styles.friendItem}>
-      <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+      {friends.map(friend => {
+        return (
+       <li key={friend.id} className={styles.friendItem}>
+      <FriendListItem avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline} />
 </li>
-
-  </>
-  ))}
+  );
+})}
     </ul>
     </div>
   )
 }
 
 export default FriendList;
-
-
-
