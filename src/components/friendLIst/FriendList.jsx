93981d06b6b5eecfe950edc8ea0aebb3b31612a3
Yuliia Shvets <y.shvets85@gmail.com>
2025-01-12 13:06:15 +0100

    add .

--- src/components/friendLIst/FriendList.jsx
+++ src/components/friendLIst/FriendList.jsx
@@ -1,30 +1,24 @@
 import styles from "./Friendlist.module.css"
+import FriendListItem from "../friendListItem/FriendListItem"
 
 const FriendList = ({friends}) => {
-    return (
-        <div className={styles.friendSection}>
-        <ul className={styles.friendList}>
-        {friends.map(({ avatar, name, isOnline }) => (
-           <> 
-	<li className={styles.friendItem}>
-        <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
-	</li>
+  return (
+      <div className={styles.friendSection}>
+      <ul className={styles.friendList}>
+      {friends.map(({ avatar, name, isOnline }) => (
+         <> 
+<li className={styles.friendItem}>
+      <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+</li>
 
-    </>
-    ))}
-      </ul>
-      </div>
-    )
+  </>
+  ))}
+    </ul>
+    </div>
+  )
 }
 
-const FriendListItem = ({ id,avatar, name, isOnline }) => {
-    return (
-      <div key={id} className={styles.friendCard}>
-        <img className={styles.friendImg}src={avatar} alt={`${name} avatar`} width="64" />
-        <p className={styles.friendName}>{name}</p>
-        <p className={`${styles.isOnline} ${isOnline ? styles.online : styles.offline}`}>{isOnline ? "Online" : "Offline"}</p>
-      </div>
-    )
-  };
-
 export default FriendList;
+
+
+
